da0121b8ac4abf0564258472b0874f7320ad1cb4
SanketD92 <sdharwa1@binghamton.edu>
2019-09-04 20:46:25 -0400

    Update Image_Identifier_Model.py

--- Image_Identifier_Model.py
+++ Image_Identifier_Model.py
@@ -18,53 +18,51 @@ y = pickle.load(pickle_in)
 
 X = X/255.0     # Normalize image data for quicker processing
 
-dense_layers = [0, 1, 2]
-layer_sizes = [32, 64, 128]       # These don't need to be in 2's power ranges, just used as an example
-conv_layers = [1, 2, 3]
+model = Sequential()
 
-for dense_layer in dense_layers:
-    for layer_size in layer_sizes:
-        for conv_layer in conv_layers:
-            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
-            print(NAME)
+# Layer 1
+# Convolution is used to find cross-correlation between the filter and the image window
+model.add(Conv2D(128, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+# After convolution, output size decreases but features identified increases number of output images, which requires sampling or pooling
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-            model = Sequential()
-            
-            # Layer 1
-            # Convolution is used to find cross-correlation between the filter and the image window
-            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
-            model.add(Activation('relu'))
-            # After convolution, output size decreases but features identified increases number of output images, which requires sampling or pooling
-            model.add(MaxPooling2D(pool_size=(2, 2)))
+# Additional Layers based on conv_layer loop
+# Deeper convolution filter identifies more complex features of the input image
 
-            # Additional Layers based on conv_layer loop
-            # Deeper convolution filter identifies more complex features of the input image
-            for l in range(conv_layer-1):
-                model.add(Conv2D(layer_size, (3, 3)))
-                model.add(Activation('relu'))
-                model.add(MaxPooling2D(pool_size=(2, 2)))
+# Layer 2
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-            # The first Dense layer should be preceded by Flatten
-            model.add(Flatten())
+# Layer 3
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-            # Dense Layers based on dense_layer loop
-            for _ in range(dense_layer):
-                model.add(Dense(layer_size))
-                model.add(Activation('relu'))
+# The first Dense layer should be preceded by Flatten
+model.add(Flatten())
 
-            model.add(Dense(1))
-            model.add(Activation('sigmoid'))
+# Dense Layers based on dense_layer loop
+model.add(Dense(128))
+model.add(Activation('relu'))
 
-            tensorboard = TensorBoard(log_dir="logs2/{}".format(NAME))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
 
-            model.compile(loss='binary_crossentropy',   # binary since data is dogs or cats
-                          optimizer='adam',
-                          metrics=['accuracy'],
-                          )
+model.compile(loss='binary_crossentropy',   # binary since data is dogs or cats
+                optimizer='adam',
+                metrics=['accuracy'],
+                )
 
-            model.fit(X, y,
-                      batch_size=50,                    # batch size depends on data size
-                      epochs=10,
-                      validation_split=0.3,
-                      callbacks=[tensorboard])
-            
+model.fit(X, y,
+            batch_size=50,                    # batch size depends on data size
+            epochs=10,
+            validation_split=0.3)
+
+model.summary()
+
+print(mode.summary())
+
+# Test
+model.predict()
